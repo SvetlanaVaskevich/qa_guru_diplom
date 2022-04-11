1a48ae9d310a9fed474285dcb79e56b1734d62a7
Svetlana Vaskevich <svaskevich@bk.ru>
2022-04-11 18:55:52 +0500

    WebDriver added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,50 +1,34 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
+
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ProjectConfig;
+import drivers.WebDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
+    static ProjectConfig config = ConfigFactory.create(ProjectConfig.class,System.getProperties());
+
     @BeforeAll
     public static void init(){
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        String browser =System.getProperty("browser");
-        String version = System.getProperty("version");
-        String login = System.getProperty("login");
-        String password = System.getProperty("password");
-        String url = System.getProperty("url");
-        String remoteUrl = "https://" + login + ":" + password + "@" + url;
-
-        Configuration.baseUrl = "https://www.chitai-gorod.ru/";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "";
-        Configuration.browserVersion = version;
-        Configuration.remote = remoteUrl;
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC",true);
-        capabilities.setCapability("enableVideo",true);
-        Configuration.browserCapabilities = capabilities;
+        WebDriver driver = new WebDriver();
+        driver.createDriver();
     }
 
     @AfterEach
     public void addAttachments() {
-        /*Attach.screenshotAs(System.getProperty("browser")+" "+System.getProperty("version"));
+        Attach.screenshotAs(System.getProperty("browser")+" "+System.getProperty("version"));
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();*/
+        Attach.addVideo();
 
         closeWebDriver();
     }
