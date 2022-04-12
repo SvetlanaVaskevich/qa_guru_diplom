dcb59897b741a21e48b34d196c684d63b6209444
Svetlana Vaskevich <svaskevich@bk.ru>
2022-04-12 07:52:03 +0500

    MainPage and TestBase edit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ProjectConfig;
 import drivers.WebDriver;
@@ -9,6 +10,8 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -20,8 +23,25 @@ public class TestBase {
     @BeforeAll
     public static void init(){
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        WebDriver driver = new WebDriver();
-        driver.createDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--lang=ru-ru");
+
+        Configuration.baseUrl = "https://www.chitai-gorod.ru/";
+        Configuration.browser = config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.browserSize = config.browserSize();
+        Configuration.remote = config.remoteUrl();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
