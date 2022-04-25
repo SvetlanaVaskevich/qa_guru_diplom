46836858b90b6630921e974d5f90ad9e03dca31f
Svetlana Vaskevich <svaskevich@bk.ru>
2022-04-25 17:43:50 +0500

    mobile _example.properties added

--- src/test/java/config/EmulatorConfig.java
+++ src/test/java/config/EmulatorConfig.java
@@ -5,14 +5,9 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:mobileConfig/emulator.properties")
 public interface EmulatorConfig extends Config {
 
-    @Key("platform.name")
     String platformName();
-    @Key("platform.version")
     String platformVersion();
-    @Key("device.name")
     String deviceName();
-    @Key("app.package")
     String appPackage();
-    @Key("app.activity")
     String appActivity();
 }
