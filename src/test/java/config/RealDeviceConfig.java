8f23f5ded8e81e0fbabaf7dd9634b7bb9c2f7b88
Svetlana Vaskevich <svaskevich@bk.ru>
2022-04-29 21:34:30 +0500

    TestBase and Attach edit

--- src/test/java/config/RealDeviceConfig.java
+++ src/test/java/config/RealDeviceConfig.java
@@ -5,14 +5,9 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:mobileConfig/realDevice.properties")
 public interface RealDeviceConfig extends Config {
 
-    @Key("platform.name")
     String platformName();
-    @Key("platform.version")
     String platformVersion();
-    @Key("device.name")
     String deviceName();
-    @Key("app.package")
     String appPackage();
-    @Key("app.activity")
     String appActivity();
 }
